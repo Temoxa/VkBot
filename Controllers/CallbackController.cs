b5e70a7219ca234a3a08517513183fc29161732f
Temoxa <senart2007@yandex.ru>
2019-06-09 20:42:35 +0700

    fix

---
+++ Controllers/CallbackController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using VkBotTest.Models;
+using VkNet.Abstractions;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+using VkNet.Utils;
+
+namespace VkBotTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallbackController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IVkApi _vkApi;
+
+        public CallbackController(IVkApi vkApi, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _vkApi = vkApi;
+        }
+
+        [HttpPost]
+        public IActionResult Callback([FromBody] Updates updates)
+        {
+            // Проверяем, что находится в поле "type" 
+            switch (updates.Type)
+            {
+                // Если это уведомление для подтверждения адреса
+                case "confirmation":
+                    // Отправляем строку для подтверждения 
+                    return Ok(_configuration["ConfigVK:Confirmation"]);
+
+                // Новое сообщение
+                case "message_new":
+                    {
+                        // Десериализация
+                        var msg = Message.FromJson(new VkResponse(updates.Object));
+
+                        // Отправим в ответ полученный от пользователя текст
+                        _vkApi.Messages.Send(new MessagesSendParams
+                        {
+                            RandomId = new DateTime().Millisecond,
+                            PeerId = msg.PeerId.Value,
+                            Message = msg.Text
+                        });
+
+                        break;
+                    }
+            }
+
+            // Возвращаем "ok" серверу Callback API
+            return Ok("ok");
+        }
+
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            return Ok("ok");
+        }
+    }
+}
